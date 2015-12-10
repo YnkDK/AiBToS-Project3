9ce242ec4cc23b308f559c9180ade69a456f4603
kostas <kmampent@cs.au.dk>
2015-12-10 11:49:30 +0100

    add hilbert and hpscore2

--- hilbert.py
+++ hilbert.py
@@ -1,4 +1,4 @@
-import hpscore
+import hpscore2
 import sys
 
 if __name__ == "__main__":
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     seq2="enwnnesenesswseeneseenwnenwwswnnneseenwnenwwswnwwsesswnwswnnenwnneseenwnenwwswnnenwnnesenesswseeenwnnesenesswsesswnwwseswseeneseneseenwnenwwswnnenwnnesenesswseeenwnnesenesswsesswnwwseswseenesswsesswnwswnnenwwswnwwseswseenessswnwwseswseeneseenwnnesenesswse"
     match = sys.argv[1]
     szMatch = len(match)
-    seq = hpscore.HPFold(match)
+    seq = hpscore2.HPFold(match)
     maxscore = 0
     for i in range(0, len(seq2)-szMatch+2):
 	seq3=seq2[i:i+szMatch-1]
         print seq3
-        absfold = hpscore.make_absfold(seq3)
-        relfold = hpscore.make_relfold(seq3)
+        absfold = hpscore2.make_absfold(seq3)
+        relfold = hpscore2.make_relfold(seq3)
         if len(absfold) == len(seq) - 1:
   	    seq.SetAbsFold(absfold)
         elif len(relfold) == len(seq) - 1:
